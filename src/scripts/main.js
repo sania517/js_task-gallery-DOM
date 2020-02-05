bb930ff4e87747fdb9aacb869ff33cb012f2e6ec
Olexander <kravchenko.sania@gmail.com>
2020-02-05 18:57:31 +0200

    attempt 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+function handler(e) {
+  event.preventDefault();
+
+  const directImage = e.target.src
+    ? e.target
+    : e.target.querySelector('img');
+
+  largeImage.src = directImage.src.replace('-thumb.jpeg', '.png');
+}
+
+thumbs.addEventListener('click', handler);
